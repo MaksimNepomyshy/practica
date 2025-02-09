8198d9e870fc6d02520b4ad2679ee91935eb5ccc
Максим Непомящий <166436244+MaksimNepomyshy@users.noreply.github.com>
2025-02-09 15:01:49 +0200

    Add files via upload

--- js/app/app.js
+++ js/app/app.js
@@ -1,5 +1,7 @@
 import {router} from './router.js';
+import {popup} from './widgets/popup.js';
 import {msg} from './widgets/msg.js';
+import {header} from './widgets/header.js';
 
 document.addEventListener('DOMContentLoaded', function(){
     const main = {
@@ -79,6 +81,8 @@ document.addEventListener('DOMContentLoaded', function(){
           };
         
     var app = Vue.createApp(main)
+    .component('Header' ,header)
+    .component('popup' ,popup)
     .component('msg' ,msg)
     .use(router)
     .mount('#content')
